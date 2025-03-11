defa98d86cb8179705c07c268699398d572e3332
Mount <1647423208@qq.com>
2025-03-11 20:06:19 +0800

    fix:全局异常拦截器的依赖

--- admin/src/main/java/com/jobhunter/admin/common/exception/AbstractException.java
+++ admin/src/main/java/com/jobhunter/admin/common/exception/AbstractException.java
@@ -4,6 +4,8 @@ import com.jobhunter.admin.common.convention.errorcode.IErrorCode;
 import lombok.Getter;
 import org.springframework.util.StringUtils;
 
+import java.util.Optional;
+
 /**
  * 抽象项目中三类异常体系，客户端异常、服务端异常以及远程服务调用异常
  *
